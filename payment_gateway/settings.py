1c06f31069470386013d27ba673b5ca2d70d4c86
Vincent Jared <vincentjared18@gmail.com>
2025-06-14 13:35:05 -0400

    Allow all hosts in production

--- payment_gateway/settings.py
+++ payment_gateway/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,mastercraft-stage2-3.onrender.com,.onrender.com').split(',')
+ALLOWED_HOSTS = ['*'] if not DEBUG else ['localhost', '127.0.0.1']
 
 
 # Application definition
